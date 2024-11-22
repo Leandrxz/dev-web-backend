8b8035ebba263cab224fc56ed69451a9d441078d
renan.beraldi <renanberaldi@outlook.com>
2024-11-22 15:07:31 -0300

    first commit / applicatio created

---
+++ src/main/java/br/com/santaclara/SantaclaraApplication.java
@@ -0,0 +1,13 @@
+package br.com.santaclara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaclaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaclaraApplication.class, args);
+	}
+
+}
